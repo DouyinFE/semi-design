1c4f2544be88a182dc88c9b30687d6931bcf1ba5
pointhalo <88709023+pointhalo@users.noreply.github.com>
2021-11-04 12:27:48 +0800

    docs: update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -15,7 +15,7 @@ export class Footer extends Component {
                     <a href={`/${getLocale()}/start/getting-started`} className='text'>{_t('footer.component')}</a>
                     <a href='https://figma.com/@semi' className='figmaUIKit' target="_blank" rel="noreferrer">Figma UIKit</a>
                     <p className='text_d3ba282e'><a href="https://semi.design/dsm/landing" className='text_8b88424e' target="_blank" rel="noreferrer">{_t('footer.dsm')}</a></p>
-                    <a href="https://github.com/DouyinFE/semi-design" className='github' target="_blank" rel="noreferrer">Github</a>
+                    <a href="https://github.com/DouyinFE/semi-design" className='github' target="_blank" rel="noreferrer">GitHub</a>
                 </div><img src="https://lf9-static.semi.design/obj/semi-tos/images/a577c310-324e-11ec-8b14-8fb159794ae4.svg" className='divider' />
                 <div className='autoWrapper'>
                     <p className='a2021SemiDesignAllRi'>© 2021 Semi Design. All rights reserved</p>
