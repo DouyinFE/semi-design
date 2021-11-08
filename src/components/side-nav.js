8675ee028b91544817f0e7515b55bf647fa52203
pointhalo <point.halo@outlook.com>
2021-11-08 14:55:48 +0800

    docs: remove props:link & linkOptions which add recently, it can't open new page when using with command + click, #220

--- src/components/side-nav.js
+++ src/components/side-nav.js
@@ -34,12 +34,12 @@ const SideNav = ({ location = null, type = null, itemsArr, edges, style, hasBann
                     text: node.frontmatter.title,
                     icon: <Icon svg={<IconNode />} size={'extra-large'} /> || '',
                     order: node.frontmatter.order || 0,
-                    link: `/${node.fields.slug}`,
-                    linkOptions: {
-                        onClick: (e) => {
-                            e.preventDefault();
-                        }
-                    },
+                    // link: `/${node.fields.slug}`,
+                    // linkOptions: {
+                    //     onClick: (e) => {
+                    //         e.preventDefault();
+                    //     }
+                    // },
                     category: navData[categoryIndex].text || navData[categoryIndex].textUs,
                 });
                 navData[categoryIndex].items.sort((a, b) => a.order - b.order);
