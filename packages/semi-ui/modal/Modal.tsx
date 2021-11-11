26e45f5da2fef467605bcdfe06ac52813f43559d
代强 <daiqianghaha@foxmail.com>
2021-11-11 16:47:05 +0800

    fix: fix modal afterClose no use after css animation

--- packages/semi-ui/modal/Modal.tsx
+++ packages/semi-ui/modal/Modal.tsx
@@ -157,9 +157,9 @@ class Modal extends BaseComponent<ModalReactProps, ModalState> {
             notifyClose: () => {
                 this.props.afterClose();
             },
-            toggleHidden: (hidden: boolean) => {
+            toggleHidden: (hidden: boolean, callback?: (hidden: boolean) => void) => {
                 if (hidden !== this.state.hidden) {
-                    this.setState({ hidden });
+                    this.setState({ hidden }, callback || noop);
                 }
             },
             notifyFullScreen: (isFullScreen: boolean) => {
@@ -173,17 +173,11 @@ class Modal extends BaseComponent<ModalReactProps, ModalState> {
     static getDerivedStateFromProps(props: ModalReactProps, prevState: ModalState) {
         const newState: Partial<ModalState> = {};
 
-        if (props.visible && prevState.hidden) {
-            newState.hidden = false;
-        }
         if (props.fullScreen !== prevState.isFullScreen) {
             newState.isFullScreen = props.fullScreen;
         }
 
-        // if not using animation, need to update hidden state from props
-        if (!props.visible && !props.motion && !prevState.hidden) {
-            newState.hidden = true;
-        }
+
         return newState;
     }
 
@@ -244,6 +238,10 @@ class Modal extends BaseComponent<ModalReactProps, ModalState> {
         if (prevProps.visible && !this.props.visible) {
             this.foundation.afterHide();
         }
+
+        if(!this.props.motion){
+            this.updateHiddenState();
+        }
     }
 
     componentWillUnmount() {
@@ -259,6 +257,16 @@ class Modal extends BaseComponent<ModalReactProps, ModalState> {
     handleOk = (e: React.MouseEvent) => {
         this.foundation.handleOk(e);
     };
+
+    updateHiddenState=()=>{
+        const {visible}=this.props;
+        const {hidden}=this.state;
+        if (!visible && !hidden) {
+            this.foundation.toggleHidden(true, () => this.foundation.afterClose());
+        } else if (visible && this.state.hidden) {
+            this.foundation.toggleHidden(false)
+        }
+    }
 
     renderFooter = (): ReactNode => {
         const {
@@ -351,7 +359,7 @@ class Modal extends BaseComponent<ModalReactProps, ModalState> {
         }
 
         const classList = cls(className, {
-            [`${cssClasses.DIALOG}-displayNone`]: keepDOM && this.state.hidden,
+            [`${cssClasses.DIALOG}-displayNone`]: keepDOM && this.state.hidden && !visible,
         });
         const contentClassName = motion ? cls({
             [`${cssClasses.DIALOG}-content-animate-hide`]: !visible,
@@ -361,18 +369,6 @@ class Modal extends BaseComponent<ModalReactProps, ModalState> {
             [`${cssClasses.DIALOG}-mask-animate-hide`]: !visible,
             [`${cssClasses.DIALOG}-mask-animate-show`]: visible
         }) : null;
-
-        const updateHiddenState=()=>{
-            if (!visible && !this.state.hidden) {
-                this.foundation.toggleHidden(true)
-            } else if (visible && this.state.hidden) {
-                this.foundation.toggleHidden(false)
-            }
-        };
-
-        if(!motion){
-            updateHiddenState()
-        }
 
         return (
             <Portal style={wrapperStyle} getPopupContainer={getPopupContainer}>
@@ -387,7 +383,7 @@ class Modal extends BaseComponent<ModalReactProps, ModalState> {
                     style={style}
                     ref={this.modalRef}
                     onAnimationEnd={() => {
-                        updateHiddenState();
+                        this.updateHiddenState();
                     }}
                     footer={renderFooter}
                     onClose={this.handleCancel}
