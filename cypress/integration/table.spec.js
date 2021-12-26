9bf9758673b06ee6c102979d315e37e8e5294924
走鹃 <imshijia@163.com>
2021-12-26 16:13:36 +0800

    test: cypress pr

--- cypress/integration/table.spec.js
+++ cypress/integration/table.spec.js
@@ -4,7 +4,6 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-
 describe('table', () => {
     it('row selection', () => {
         cy.visit('http://127.0.0.1:6009/iframe.html?id=table--selection-table&args=&viewMode=story');
