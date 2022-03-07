928b751ebfb4bfbf3d7cb4c63e21af39d26b4bb6
DaiQiangReal <daiqianghaha@foxmail.com>
2022-03-07 21:09:41 +0800

    chore: backup

--- packages/semi-scss-to-css-var/src/bin.ts
+++ packages/semi-scss-to-css-var/src/bin.ts
@@ -5,6 +5,7 @@ import { transScssVariables2CssVariables } from './index';
 
 const main = () => {
     console.log('bin exec');
+    transScssVariables2CssVariables({targetPath:"../semi-foundation",resultPath:""});
 };
 
 main();
--- packages/semi-scss-to-css-var/src/index.ts
+++ packages/semi-scss-to-css-var/src/index.ts
@@ -1,13 +1,51 @@
 import path from 'path';
 import fs from 'fs-extra';
-
+import postcss from "postcss";
+import postcssScss from 'postcss-scss';
 
 export interface Options {
-    path:string
+    targetPath: string,
+    resultPath: string
 }
 
-const transScssVariables2CssVariables = ({path}: Options) => {
-    console.log(path);
+
+const transScssVariables2CssVariables = ({targetPath, resultPath}: Options) => {
+
+
+    const findScssFiles = (filePath: string): string[] => {
+
+        const isScss = (filePath: string) => path.extname(filePath) === '.scss';
+
+        const stat = fs.statSync(filePath);
+        if (stat.isFile()) {
+            if (isScss(filePath)) {
+                return [filePath];
+            } else {
+                return [];
+            }
+        }
+        const scssFilePaths: string[] = [];
+        const fileList = fs.readdirSync(filePath);
+        fileList.forEach(filename => {
+            const stat = fs.statSync(path.join(filePath, filename));
+            if (stat.isFile()) {
+                if (isScss(filePath)) {
+                    scssFilePaths.push(filePath);
+                }
+            } else {
+                scssFilePaths.push(...findScssFiles(filePath));
+            }
+        });
+        return scssFilePaths;
+    }
+
+    const scssFileList = findScssFiles(targetPath)
+
+    console.log(scssFileList);
+
+    // postcss().process('', {syntax: postcssScss}).
+
+
 };
 
 
