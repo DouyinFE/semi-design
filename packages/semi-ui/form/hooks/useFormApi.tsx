681d4a8dcdeb9c711579c9c4e80ed52cc7d5c7ad
pointhalo <point.halo@outlook.com>
2022-03-25 15:16:46 +0800

    chore: lint useFormApi

--- packages/semi-ui/form/hooks/useFormApi.tsx
+++ packages/semi-ui/form/hooks/useFormApi.tsx
@@ -2,6 +2,6 @@ import { BaseFormApi } from '@douyinfe/semi-foundation/form/interface';
 import React, { useContext } from 'react';
 import { FormApiContext } from '../context';
  
-export default function useFormApi<T extends Record<string, any> = any >() {
+export default function useFormApi<T extends Record<string, any> = any>() {
     return useContext<BaseFormApi<T>>(FormApiContext);
 }
