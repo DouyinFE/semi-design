845e59b7415cc99dee0c9a3dfb9454577affaa4d
linyan <linyan.19960622@bytedance.com>
2022-06-22 22:01:00 +0800

    test: skip adjustPosIfNeed test

--- cypress/integration/tooltip.spec.js
+++ cypress/integration/tooltip.spec.js
@@ -100,7 +100,7 @@ describe('tooltip', () => {
         cy.get(input).should('be.focused');
     });
 
-    it('adjustPosIfNeed', () => {
+    it.skip('adjustPosIfNeed', () => {
         const viewportWidth = 400;
         const viewportHeight = 200;
         const topAndLeft = ['topLeft', 'top', 'topRight', 'leftTop', 'left', 'leftBottom'];
