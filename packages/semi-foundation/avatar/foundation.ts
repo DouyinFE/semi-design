4ecea40844a8df569c24f6bee7068f73a4a242b8
zhangyumei.0319 <zhangyumei.0319@bytedance.com>
2022-06-27 16:39:25 +0800

    feat(a11y): avatar add focus & keyboard event

--- packages/semi-foundation/avatar/foundation.ts
+++ packages/semi-foundation/avatar/foundation.ts
@@ -4,6 +4,7 @@ export interface AvatarAdapter<P = Record<string, any>, S = Record<string, any>>
     notifyImgState(isImgExist: boolean): void;
     notifyLeave(event: any): void;
     notifyEnter(event: any): void;
+    setFocusVisible: (focusVisible: boolean) => void;
 }
 
 export default class AvatarFoundation<P = Record<string, any>, S = Record<string, any>> extends BaseFoundation<AvatarAdapter<P, S>, P, S> {
@@ -30,6 +31,21 @@ export default class AvatarFoundation<P = Record<string, any>, S = Record<string
 
     handleLeave(e: any) {
         this._adapter.notifyLeave(e);
+    }
+
+    handleFocusVisible = (event: any) => {
+        const { target } = event;
+        try {
+            if (target.matches(':focus-visible')) {
+                this._adapter.setFocusVisible(true);
+            }
+        } catch (error){
+            console.warn('The current browser does not support the focus-visible');
+        }
+    }
+
+    handleBlur = () => {
+        this._adapter.setFocusVisible(false);
     }
 
 }
