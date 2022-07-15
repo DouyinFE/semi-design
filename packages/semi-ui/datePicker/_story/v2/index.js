e8378f360ce5bf398bad2cf4cb9709ffea3a5fe8
走鹃 <imshijia@163.com>
2022-07-15 11:29:53 +0800

    fix: datePicker triggerRender disabled bug #676

--- packages/semi-ui/datePicker/_story/v2/index.js
+++ packages/semi-ui/datePicker/_story/v2/index.js
@@ -4,3 +4,4 @@ export { default as FixInputRangeFocus } from './FixInputRangeFocus';
 export { default as InsetInput  } from './InsetInput';
 export { default as InsetInputE2E  } from './InsetInputE2E';
 export { default as FixDefaultPickerValue } from './FixDefaultPickerValue';
+export { default as FixedTriggerRender } from './FixTriggerRender';
