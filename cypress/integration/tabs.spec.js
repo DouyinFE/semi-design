0c16cceac9acf593bc83de5dbf3d85b326624f3a
linyan <linyan.19960622@bytedance.com>
2022-08-25 20:37:03 +0800

    fix: make collapse icon fixed to avoid the collapse tabs flush in some width

--- cypress/integration/tabs.spec.js
+++ cypress/integration/tabs.spec.js
@@ -23,16 +23,16 @@ describe('tabs', () => {
         cy.get('.semi-tabs-content').contains('关于');
     });
 
-    it('collapse', () => {
+    it.only('collapse', () => {
         cy.visit('http://127.0.0.1:6006/iframe.html?id=tabs--collapse-tabs&args=&viewMode=story');
         cy.viewport(800, 800);
         cy.get('.semi-tabs-content').eq(0).contains('Content of card tab 0');
-        cy.get('.semi-button').eq(0).trigger('mouseover');
+        cy.get('.semi-button').eq(1).trigger('mouseover');
         cy.get('.semi-dropdown').contains('Tab-6').click();
         cy.get('.semi-tabs-content').eq(0).contains('Content of card tab 6');
 
         // Tab-10 visible
-        cy.get('.semi-button').eq(0).click();
+        cy.get('.semi-button').eq(0).click({ force: true });
         cy.get('.semi-tabs-tab').contains('Tab-10').click({ force: true });
         cy.get('.semi-tabs-content').eq(0).contains('Content of card tab 10');
     });
