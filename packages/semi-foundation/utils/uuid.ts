f1a3b69067b9b7d3af5d4ec413fc7ba77929d3dd
代强 <daiqianghaha@foxmail.com>
2022-10-11 16:37:06 +0800

    feat: use crypto.randomUUID

--- packages/semi-foundation/utils/uuid.ts
+++ packages/semi-foundation/utils/uuid.ts
@@ -5,7 +5,9 @@ export default function getUuid(prefix: string) {
 // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
 function getUuidv4() {
     try {
-        return crypto.randomUUID();
+        return crypto?.randomUUID?.() ?? (String(1e7 + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+            (Number(c) ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (Number(c) / 4)))).toString(16)
+        ));
     } catch (err) {
         return getUuid('semi');
     }
