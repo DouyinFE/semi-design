08616dddd9cb72976d63ff21db041fc95e3ce740
linyan <linyan.19960622@bytedance.com>
2022-10-19 14:28:16 +0800

    fix: fix cypress faild

--- cypress/integration/datePicker.spec.js
+++ cypress/integration/datePicker.spec.js
@@ -536,19 +536,19 @@ describe('DatePicker', () => {
     it('cashedSelectedValue after selected date', () => {
         cy.visit('http://localhost:6006/iframe.html?id=datepicker--cashed-selected-value&viewMode=story');
         cy.get('[data-cy=date] .semi-input').first().click();
-        cy.get('.semi-datepicker-day').contains("5").click();
+        cy.get('.semi-datepicker-day').contains("5").click({ force: true });
         cy.get('[data-cy=date] .semi-input').first().click();
         cy.get('.semi-popover .semi-datepicker-day-selected').contains('5');
 
         cy.get('[data-cy=dateTime] .semi-input').first().click();
-        cy.get('.semi-datepicker-day').contains("5").click();
+        cy.get('.semi-datepicker-day').contains("5").click({ force: true });
         cy.get('[data-cy=dateTime] .semi-input').first().click();
         cy.get('.semi-popover .semi-datepicker-day-selected').contains('5');
 
         cy.get('[data-cy=dateRange] .semi-input').eq(0).click();
-        cy.get('.semi-datepicker-day').contains("5").click();
+        cy.get('.semi-datepicker-day').contains("5").click({ force: true });
         cy.get('[data-cy=dateRange] .semi-input').eq(1).click();
-        cy.get('.semi-datepicker-day').contains("20").click();
+        cy.get('.semi-datepicker-day').contains("20").click({ force: true });
         cy.get('[data-cy=dateRange] .semi-input').eq(0).click();
         cy.get('.semi-popover .semi-datepicker-day-selected-start').contains('5');
         cy.get('.semi-popover .semi-datepicker-day-selected-end').contains('20');
