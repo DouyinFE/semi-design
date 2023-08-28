ea30dccb7559846b2e4e2fa56a19436f76fc656e
代强 <daiqianghaha@foxmail.com>
2023-08-28 14:17:05 +0800

    fix: cleartimeout memory leak (#1785)

--- packages/semi-foundation/navigation/subNavFoundation.ts
+++ packages/semi-foundation/navigation/subNavFoundation.ts
@@ -52,7 +52,9 @@ export default class SubNavFoundation<P = Record<string, any>, S = Record<string
         this._timer = null;
     }
 
-    destroy() {} // eslint-disable-line
+    destroy() {
+        this.clearDelayTimer();
+    }
 
     clearDelayTimer() {
         if (this._timer) {
