c6023bcf9a615fed5f597a3fc18c90f443e5b29f
zhangyumei.0319 <zhangyumei.0319@bytedance.com>
2023-10-25 11:56:45 +0800

    chore: add e2e test wait time, avoid online test error

--- cypress/e2e/typography.spec.js
+++ cypress/e2e/typography.spec.js
@@ -93,6 +93,7 @@ describe('typography', () => {
         cy.viewport(800, 1000);
         cy.visit('http://127.0.0.1:6006/iframe.html?id=typography--custom-tooltip&args=&viewMode=story');
         cy.get('.semi-typography').trigger('mouseover');
+        cy.wait(2000);
         cy.get('.semi-tooltip-wrapper').eq(0).should('have.attr', 'style').should('contain', 'background-color: blue');
     });
 
@@ -100,6 +101,7 @@ describe('typography', () => {
         cy.viewport(800, 1000);
         cy.visit('http://127.0.0.1:6006/iframe.html?id=typography--global-ellipsis-popover-cls&args=&viewMode=story');
         cy.get('.semi-typography').trigger('mouseover');
+        cy.wait(2000);
         cy.get('.testPopoverCls.semi-typography-ellipsis-popover').should('exist');
     });
 
