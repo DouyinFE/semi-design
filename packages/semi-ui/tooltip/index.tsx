b9081cf7861fd5ca33b60392213ce1f30c3177b9
nekocode <nekocode.cn@gmail.com>
2023-12-02 21:05:18 +0800

    fix: fix flickering of select in safari

--- packages/semi-ui/tooltip/index.tsx
+++ packages/semi-ui/tooltip/index.tsx
@@ -240,6 +240,7 @@ export default class Tooltip extends BaseComponent<TooltipProps, TooltipState> {
                         isInsert: true,
                         transitionState: 'enter',
                         containerStyle: { ...this.state.containerStyle, ...containerStyle },
+                        isPositionUpdated: false,
                     },
                     () => {
                         setTimeout(() => {
