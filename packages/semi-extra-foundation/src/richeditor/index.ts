564015fed22aaf0a4390140a1d21fbc1b4bcfb35
代强 <daiqianghaha@foxmail.com>
2023-12-20 17:45:46 +0800

    chore: backup

--- packages/semi-extra-foundation/src/richeditor/index.ts
+++ packages/semi-extra-foundation/src/richeditor/index.ts
@@ -1,7 +1,7 @@
 import BaseFoundation, { DefaultAdapter } from "@douyinfe/semi-foundation/lib/es/base/foundation";
 import { v4 } from 'uuid';
 import "./index.scss";
-import { cloneDeep, nth } from "lodash-es";
+import { isEqual, nth } from "lodash-es";
 
 
 export interface RichEditorProps{
@@ -51,8 +51,8 @@ export interface EditorTypeColor extends EditorType{
 }
 
 export interface EditorTypeBgColor extends EditorType{
-    name: 'bgColor'
-
+    name: 'bgColor';
+    value: string
 }
 
 export interface EditorTypeLink extends EditorType{
@@ -65,21 +65,27 @@ export interface EditorTypeText extends EditorType{
     value?: number
 }
 
-export type EditorTypes = EditorTypeBold|EditorTypeItalic|EditorTypeTitle|EditorTypeUnderline|EditorTypeColor|EditorTypeBgColor|EditorTypeLink|EditorTypeText
+export interface EditorTypeNewLine extends EditorType{
+    name: 'newLine'
+}
+
+export type EditorTypes = EditorTypeBold|EditorTypeItalic|EditorTypeTitle|EditorTypeUnderline|EditorTypeColor|EditorTypeBgColor|EditorTypeLink|EditorTypeText|EditorTypeNewLine
 
 export interface RichEditorAdapter<P = Record<string, any>, S = Record<string, any>> extends DefaultAdapter<P, S> {
     createElement: (node: EditorNode, index: number) => Promise<void>;
     updateElement: (node: EditorNode) => Promise<void>;
     getDOMByNodeId: (id: string) => HTMLElement|null;
-    getActiveNodeId: () => string|null;
-    replaceElement: (node: EditorNode, newNodes: EditorNode[]) => Promise<void>
+    getSelectionInfo: () => {startNodeId: string|null; endNodeId: string|null; startOffset: number; endOffset: number };
+    replaceElement: (startIndex: number, endIndex: number, removeNodes: EditorNode[], newNodes: EditorNode[]) => Promise<void>;
+    insertElement: (index: number, nodes: EditorNode[]) => Promise<void>
 }
-
+ 
 
 export interface EditorNode{
     id: string;
     content: string;
-    type: EditorTypes[]
+    type: EditorTypes[];
+    special?: string
 }
 
 
@@ -144,11 +150,28 @@ class RichEditorFoundation <P = Record<string, any>, S = Record<string, any>> ex
         }
     }
 
+    onUserInsertParagraph = async (nodeId: string|null)=>{
+        const node = this.createNode("\n", [{
+            name: 'newLine',
+        } as EditorTypeNewLine]);
+        if (nodeId===null) {
+            await this._adapter.createElement(node, this.foundationState.data.nodes.length);
+        } else {
+            const index = this.foundationState.data.nodes.findIndex((item)=>item.id===nodeId);
+            if (index===-1) {
+                return;
+            }
+            await this._adapter.createElement(node, index+1);
+        }
+        this.idNodeMap.set(node.id, node);
+        this.foundationState.data.nodes.push(node);
+    }
+
     createNode = (data: string, types: EditorNode['type'])=>{
         return {
             id: v4(),
             content: data,
-            type: types
+            type: types,
         };
     }
 
@@ -157,70 +180,249 @@ class RichEditorFoundation <P = Record<string, any>, S = Record<string, any>> ex
     }
 
 
-    onContainerClick = ()=>{
-        const lastNode = nth(this.foundationState.data.nodes, -1);
-        if (lastNode) {
-            this._adapter.getDOMByNodeId(lastNode.id)?.focus();
+    onContainerClick = (e: PointerEvent)=>{
+        const id = (e.target as HTMLElement)?.getAttribute('data-node-id');
+        if (id) {
+            const node = this.getNodeById(id);
+            if (node) {
+                this._adapter.getDOMByNodeId(node.id)?.focus();
+            }
+        } else {
+            const lastNode = nth(this.foundationState.data.nodes, -1);
+            if (lastNode) {
+                this._adapter.getDOMByNodeId(lastNode.id)?.focus();
+            }
         }
+    }
+
+    applyTypeToNodes = async ({ startNode, endNode, offsetInEndNode, offsetInStartNode, newTypes }: {
+        startNode: EditorNode; endNode: EditorNode;offsetInStartNode: number;offsetInEndNode: number;newTypes: EditorTypes[]
+    })=>{
+        // buggy
+        const startNodeIndex = this.foundationState.data.nodes.findIndex((item)=>item.id===startNode.id);
+        const endNodeIndex = this.foundationState.data.nodes.findIndex((item)=>item.id===endNode.id);
+
+
+        const newStartNode = this.createNode(startNode.content.slice(0, offsetInStartNode), startNode.type);
+        const newEndNode = this.createNode(endNode.content.slice(offsetInEndNode), endNode.type);
+
+        let nodeSplitFromStartNode;
+        let nodeSplitFromEndNode;
+        if (startNode.id===endNode.id) {
+            nodeSplitFromStartNode = this.createNode(startNode.content.slice(offsetInStartNode, offsetInEndNode), [...startNode.type, ...newTypes]);
+            nodeSplitFromEndNode = this.createNode("", [...endNode.type, ...newTypes]);
+        } else {
+            nodeSplitFromStartNode = this.createNode(startNode.content.slice(offsetInStartNode), [...startNode.type, ...newTypes]);
+            nodeSplitFromEndNode = this.createNode(endNode.content.slice(0, offsetInEndNode), [...endNode.type, ...newTypes]);
+        }
+
+        const middleNodes = this.foundationState.data.nodes.slice(startNodeIndex + 1, endNodeIndex);
+        const newMiddleNodes = middleNodes.map((item)=>{
+            return this.createNode(item.content, [...item.type, ...newTypes]);
+        });
+
+        const willAddedNodes = [newStartNode, nodeSplitFromStartNode, ...newMiddleNodes, nodeSplitFromEndNode, newEndNode];
+        const willAddedNodesWithContent: EditorNode[] = [];
+        willAddedNodes.forEach((item)=>{
+            if (item.content.length===0) {
+                this.idNodeMap.delete(item.id);
+            } else {
+                willAddedNodesWithContent.push(item);
+                this.idNodeMap.set(item.id, item);
+            }
+        });
+        
+        this.foundationState.data.nodes.splice(startNodeIndex, endNodeIndex - startNodeIndex + 1, ...willAddedNodesWithContent);
+        await this._adapter.replaceElement(startNodeIndex, endNodeIndex, [startNode, ...middleNodes, endNode], willAddedNodesWithContent);
+        // await this.optimizeNodes(startNodeIndex, startNodeIndex - (endNodeIndex - startNodeIndex + 1) + willAddedNodesWithContent.length);
     }
 
 
     onTitle = (titleNum: number)=>{
 
     }
-    onBold = ()=>{
-        const id = this._adapter.getActiveNodeId();
-        if (!id) {
+    onBold = async ()=>{
+        const { startNodeId, endNodeId, startOffset, endOffset } = this._adapter.getSelectionInfo();
+        if (!startNodeId || !endNodeId) {
             return;
         }
-        const node = this.getNodeById(id);
-        if (!node) {
+        const startNode = this.getNodeById(startNodeId);
+        const endNode = this.getNodeById(endNodeId);
+        if (!startNode || !endNode) {
             return;
         }
-        const selectStart = window.getSelection()?.anchorOffset;
-        const selectEnd = window.getSelection()?.focusOffset;
-        if (selectStart===selectEnd) {
-            return;
-        }
-        const oldNodeContent = node.content.slice(0, selectStart);
-        const midNodeContent = node.content.slice(selectStart, selectEnd);
-        const lastNodeContent = node.content.slice(selectEnd);
 
-        node.content = oldNodeContent;
-        const midNode = this.createNode(midNodeContent, [{
-            name: 'bold'
-        } as EditorTypeBold, ...node.type]);
-        const lastNode = {
-            ...cloneDeep(node),
-            content: lastNodeContent
-        };
-
-        const nodeIndex = this.foundationState.data.nodes.findIndex((item)=>item.id===node.id);
-        this.foundationState.data.nodes.splice(nodeIndex, 1, node, midNode, lastNode);
-        this._adapter.replaceElement(node, [node, midNode, lastNode]);
-
+        await this.applyTypeToNodes({
+            startNode,
+            endNode,
+            offsetInStartNode: startOffset,
+            offsetInEndNode: endOffset,
+            newTypes: [{
+                name: 'bold'
+            } as EditorTypeBold]
+        });
 
         
     }
 
-    onItalic = ()=>{
+    onItalic = async ()=>{
+        const { startNodeId, endNodeId, startOffset, endOffset } = this._adapter.getSelectionInfo();
+        if (!startNodeId || !endNodeId) {
+            return;
+        }
+        const startNode = this.getNodeById(startNodeId);
+        const endNode = this.getNodeById(endNodeId);
+        if (!startNode || !endNode) {
+            return;
+        }
 
+        await this.applyTypeToNodes({
+            startNode,
+            endNode,
+            offsetInStartNode: startOffset,
+            offsetInEndNode: endOffset,
+            newTypes: [{
+                name: 'italic'
+            } as EditorTypeItalic]
+        });
     }
 
-    onUnderline = ()=>{
+    onUnderline = async ()=>{
+        const { startNodeId, endNodeId, startOffset, endOffset } = this._adapter.getSelectionInfo();
+        if (!startNodeId || !endNodeId) {
+            return;
+        }
+        const startNode = this.getNodeById(startNodeId);
+        const endNode = this.getNodeById(endNodeId);
+        if (!startNode || !endNode) {
+            return;
+        }
 
+        await this.applyTypeToNodes({
+            startNode,
+            endNode,
+            offsetInStartNode: startOffset,
+            offsetInEndNode: endOffset,
+            newTypes: [{
+                name: 'underline'
+            } as EditorTypeUnderline]
+        });
     }
 
-    onColor = (color: string)=>{
+    onColor = async (color: string)=>{
+        const { startNodeId, endNodeId, startOffset, endOffset } = this._adapter.getSelectionInfo();
+        if (!startNodeId || !endNodeId) {
+            return;
+        }
+        const startNode = this.getNodeById(startNodeId);
+        const endNode = this.getNodeById(endNodeId);
+        if (!startNode || !endNode) {
+            return;
+        }
 
+        await this.applyTypeToNodes({
+            startNode,
+            endNode,
+            offsetInStartNode: startOffset,
+            offsetInEndNode: endOffset,
+            newTypes: [{
+                name: 'color',
+                value: color
+            } as EditorTypeColor]
+        });
     }
 
-    onBgColor = (color: string)=>{
+    onBgColor = async (color: string)=>{
+        const { startNodeId, endNodeId, startOffset, endOffset } = this._adapter.getSelectionInfo();
+        if (!startNodeId || !endNodeId) {
+            return;
+        }
+        const startNode = this.getNodeById(startNodeId);
+        const endNode = this.getNodeById(endNodeId);
+        if (!startNode || !endNode) {
+            return;
+        }
 
+        await this.applyTypeToNodes({
+            startNode,
+            endNode,
+            offsetInStartNode: startOffset,
+            offsetInEndNode: endOffset,
+            newTypes: [{
+                name: 'bgColor',
+                value: color
+            } as EditorTypeBgColor]
+        });
     }
 
     onLink = (url: string)=>{
 
+    }
+
+
+    private optimizeNodes = async (startIndex: number, endIndex: number)=>{
+        // optimize node in [startIndex,endIndex]
+
+        const newNodes: EditorNode[] = [];
+        const newNodesIdSet = new Set<string>();
+        let forwardCount = 0;
+        for (let i=startIndex;i<=endIndex;i++) {
+            const currentNode = this.foundationState.data.nodes[i];
+            const prevNode = this.foundationState.data.nodes[i-1-forwardCount];
+            if (!currentNode) {
+                break;
+            }
+
+            if (!prevNode) {
+                if (!newNodesIdSet.has(currentNode.id)) {
+                    newNodes.push(currentNode);
+                    newNodesIdSet.add(currentNode.id);
+                    continue;
+                }
+            }
+
+            if (i===startIndex) {
+                newNodes.push(prevNode);
+                newNodesIdSet.add(prevNode.id);
+                if (isEqual(currentNode.type, prevNode.type)) {
+                    prevNode.content += currentNode.content;
+                    forwardCount++;
+                } else {
+                    newNodes.push(currentNode);
+                    newNodesIdSet.add(currentNode.id);
+                }
+                continue;
+            }
+
+            if (isEqual(currentNode.type, prevNode.type)) {
+                prevNode.content += currentNode.content;
+                if (!newNodesIdSet.has(prevNode.id)) {
+                    newNodesIdSet.add(prevNode.id);
+                    newNodes.push(prevNode);
+                }
+                forwardCount++;
+            } else {
+                newNodesIdSet.add(currentNode.id);
+                newNodes.push(currentNode);
+                forwardCount=0;
+            }
+        }
+
+        const slice = (arr: any[], startIndex: number, endIndex: number = Infinity)=>{
+            if (startIndex<0) {
+                startIndex = 0;
+            }
+            if (endIndex < 0) {
+                return [];
+            }
+            endIndex = Math.min(endIndex, arr.length); 
+            return arr.slice(startIndex, endIndex);
+        };
+        const removeNodes = this.foundationState.data.nodes.slice( Math.max(startIndex - 1, 0), Math.min(this.foundationState.data.nodes.length, endIndex + 1));
+        const replaceElementArgs = [Math.max(startIndex - 1, 0), Math.min(this.foundationState.data.nodes.length, endIndex + 1), removeNodes, newNodes];
+        this.foundationState.data.nodes = [...slice(this.foundationState.data.nodes, 0, startIndex-1), ...newNodes, ...slice(this.foundationState.data.nodes, endIndex+1)];
+        await this._adapter.replaceElement(...replaceElementArgs);
+        return newNodes;
     }
 
 
--- packages/semi-extra-ui/src/richeditor/index.tsx
+++ packages/semi-extra-ui/src/richeditor/index.tsx
@@ -1,8 +1,8 @@
-import React, { ReactNode } from 'react';
+import React, { CSSProperties, ReactNode } from 'react';
 import BaseComponent from "../base";
 import RichEditorFoundation, { EditorNode, RichEditorAdapter } from "@douyinfe/semi-extra-foundation/src/richeditor";
-import { AnchorAdapter } from "@douyinfe/semi-foundation/anchor/foundation";
 import { Button } from "@douyinfe/semi-ui";
+import { nth } from "lodash-es";
 
 
 export interface RichEditorProps{
@@ -10,7 +10,9 @@ export interface RichEditorProps{
 }
 
 export interface RichEditorState{
-    elementList: ReactNode[]
+    elementList: ReactNode[];
+    color: string;
+    bgColor: string
 }
 
 class RichEditor extends BaseComponent<RichEditorProps, RichEditorState> {
@@ -18,53 +20,69 @@ class RichEditor extends BaseComponent<RichEditorProps, RichEditorState> {
     constructor(props: RichEditorProps) {
         super(props);
         this.state = {
-            elementList: []
+            elementList: [],
+            color: "#000000",
+            bgColor: "#ffffff"
         };
         this.foundation = new RichEditorFoundation(this.adapter);
+        window.test = this;
     }
     containerRef: React.RefObject<HTMLDivElement> = React.createRef();
-    nodeElementMap: Map<EditorNode, ReactNode> = new Map();
-    nodeDOMMap: Map<EditorNode, HTMLElement> = new Map()
+    nodeIdElementMap: Record<string, ReactNode> = {};
+    nodeIdDOMMap: Record<string, HTMLElement> = {};
     get adapter(): RichEditorAdapter<RichEditorProps, RichEditorState> {
         return {
             ...super.adapter,
-            replaceElement: async (node: EditorNode, newNodes: EditorNode[])=>{
-                const oldElement = this.nodeElementMap.get(node);
-                if (oldElement) {
-                    const index = this.state.elementList.indexOf(oldElement);
+            insertElement: async (index: number, nodes: EditorNode[])=>{
+                const newElements = nodes.map(this.nodeToElement);
+                const eleList = [...this.state.elementList];
+                eleList.splice(index, 0, ...newElements);
+                nodes.forEach((n, i)=>{
+                    this.nodeIdElementMap[n.id] = newElements[i];
+                });
+                return new Promise<void>(resolve => {
+                    this.setState({ elementList: eleList }, resolve);
+                });
+            },
+            replaceElement: async (startIndex: number, endIndex: number, removeNodes: EditorNode[], newNodes: EditorNode[])=>{  
+                const oldElements = this.state.elementList.slice(startIndex, endIndex+1);
+                if (oldElements.length>0) {
+                    removeNodes.forEach(node=>{
+                        delete this.nodeIdElementMap[node.id];
+                        delete this.nodeIdDOMMap[node.id];
+                    });
                     const newElements = newNodes.map(this.nodeToElement);
-                    if (index!==-1) {
-                        const eleList = [...this.state.elementList];
-                        eleList.splice(index, 1, ...newElements);
-                        this.nodeElementMap.delete(node);
-                        newNodes.forEach((n, i)=>{
-                            this.nodeElementMap.set(n, newElements[i]);
-                        });
-                        return new Promise(resolve => {
-                            this.setState({ elementList: eleList }, resolve);
-                        });
-                    }
+                    const eleList = [...this.state.elementList];
+                    eleList.splice(startIndex, endIndex-startIndex+1, ...newElements);
+
+                    newNodes.forEach((n, i)=>{
+                        this.nodeIdElementMap[n.id] = newElements[i];
+                    });
+                    return new Promise<void>(resolve => {
+                        this.setState({ elementList: eleList }, resolve);
+                    });
                 }
             },
             createElement: async (node: EditorNode, index: number)=>{
+                // insert node on index
                 const element = this.nodeToElement(node);
                 const eleList = [...this.state.elementList];
                 eleList.splice(index, 0, element);
-                this.nodeElementMap.set(node, element);
+                this.nodeIdElementMap[node.id] = element;
                 return new Promise(resolve => {
                     this.setState({ elementList: eleList }, resolve);
                 });
             },
             updateElement: async (node: EditorNode)=>{
-                const oldElement = this.nodeElementMap.get(node);
+                const oldElement = this.nodeIdElementMap[node.id];
                 if (oldElement) {
                     const newElement = this.nodeToElement(node);
                     const index = this.state.elementList.indexOf(oldElement);
                     if (index!==-1) {
                         const eleList = [...this.state.elementList];
                         eleList.splice(index, 1, newElement);
-                        this.nodeElementMap.delete(node);
-                        this.nodeElementMap.set(node, newElement);
+                        delete this.nodeIdElementMap[node.id];
+                        this.nodeIdElementMap[node.id] = newElement;
                         return new Promise(resolve => {
                             this.setState({ elementList: eleList }, resolve);
                         });
@@ -72,39 +90,79 @@ class RichEditor extends BaseComponent<RichEditorProps, RichEditorState> {
                 }
             },
             getDOMByNodeId: (id: string)=>{
-                const node = this.foundation.getNodeById(id);
-                if (node) {
-                    return this.nodeDOMMap.get(node) || null;
-                } else {
-                    return null;
-                }
+                return this.nodeIdDOMMap[id] || null;
 
             },
-            getActiveNodeId: ()=>{
-                const dom = window.getSelection()?.anchorNode?.parentElement;
-                const id = dom?.getAttribute("data-node-id");
-                return id||null;
+            getSelectionInfo: ()=>{
+                const anchorDOM = window.getSelection()?.anchorNode?.parentElement;
+                const anchorNodeId = anchorDOM?.getAttribute("data-node-id");
+                const focusDOM = window.getSelection()?.focusNode?.parentElement;
+                const focusNodeId = focusDOM?.getAttribute("data-node-id");
+                const anchorOffset = window.getSelection()?.anchorOffset;
+                const focusOffset = window.getSelection()?.focusOffset;
+
+                let anchorIndex: number;
+                let focusIndex: number;
+                this.foundation.foundationState.data.nodes.forEach((n, i)=>{
+                    if (n.id===anchorNodeId) {
+                        anchorIndex = i;
+                    }
+                    if (n.id===focusNodeId) {
+                        focusIndex = i;
+                    }
+                });
+                if (anchorIndex! < focusIndex!) {
+                    return {
+                        startNodeId: anchorNodeId!,
+                        endNodeId: focusNodeId!,
+                        startOffset: anchorOffset!,
+                        endOffset: focusOffset!
+                    };
+                } else if (anchorIndex! > focusIndex!) {
+                    return {
+                        startNodeId: focusNodeId!,
+                        endNodeId: anchorNodeId!,
+                        startOffset: focusOffset!,
+                        endOffset: anchorOffset!
+                    };
+                } else {
+                    return {
+                        startNodeId: anchorNodeId!,
+                        endNodeId: anchorNodeId!,
+                        startOffset: Math.min(anchorOffset!, focusOffset!),
+                        endOffset: Math.max(anchorOffset!, focusOffset!)
+                    };
+                }
             }
         };
-    }
+    } 
 
     private nodeToElement = (node: EditorNode): ReactNode => {
         const closure: {
             dom: HTMLDivElement|null;
             eventMounted: boolean
         } = {
-            dom: this.nodeDOMMap.get(node) as HTMLDivElement || null,
-            eventMounted: this.nodeElementMap.has(node) || false
+            dom: this.nodeIdDOMMap[node.id] as HTMLDivElement || null,
+            eventMounted: Boolean(this.nodeIdElementMap[node.id]) || false
         };
+
+        const style: CSSProperties= {};
+        node.type.forEach(t=>{
+            if (t.name==="color") {
+                style['color'] = t.value;
+            }
+            if (t.name==="bgColor") { 
+                style['backgroundColor'] = t.value;
+            }
+        });
         return <div ref={r=>{
             if (r && !closure.eventMounted) {
                 closure.dom = r;
-                this.nodeDOMMap.set(node, closure.dom);
+                this.nodeIdDOMMap[node.id] = closure.dom;
                 closure.dom.addEventListener("beforeinput", (e)=>this.handleBeforeInputEvent(e as InputEvent, closure.dom!));
                 closure.eventMounted=true;
             }
-
-        }} data-node-id={node.id} contentEditable={true} suppressContentEditableWarning={true} className={node.type.map(t=>{
+        }} data-node-id={node.id} contentEditable={true} suppressContentEditableWarning={true} style={style} className={node.type.map(t=>{
             return `semi-rich-editor-type-${t.name}`; // TODO: process each type
         }).join(" ")}>{node.content}</div>;
     }
@@ -114,17 +172,39 @@ class RichEditor extends BaseComponent<RichEditorProps, RichEditorState> {
         const containerDOM = this.containerRef.current!;
         this.foundation.registerContainer(containerDOM);
         containerDOM.addEventListener("beforeinput", (e)=>this.handleBeforeInputEvent(e as InputEvent, containerDOM));
-
+        containerDOM.addEventListener("click", this.foundation.onContainerClick);
     }
 
     handleBeforeInputEvent = (e: InputEvent, ele: HTMLDivElement)=>{
+        debugger;
+        if (e.inputType==="insertText") {
+            if (ele===this.containerRef.current) {
+                if (this.state.elementList.length===0) {
+                    this.foundation.onUserInput(null, e.data);
+                } else {
+                    const lastNode = nth(this.foundation.foundationState.data.nodes, -1) as EditorNode;
+                    this.foundation.onUserInput(lastNode.id, e.data);
+                }
 
-        if (ele===this.containerRef.current) {
-            this.foundation.onUserInput(null, e.data);
-        } else {
-            const nodeId = ele.getAttribute("data-node-id");
-            this.foundation.onUserInput(nodeId, e.data);
+            } else {
+                const nodeId = ele.getAttribute("data-node-id");
+                this.foundation.onUserInput(nodeId, e.data);
+            }
+        } else if (e.inputType==="insertParagraph") {
+            if (ele===this.containerRef.current) {
+                if (this.state.elementList.length===0) {
+                    this.foundation.onUserInsertParagraph(null);
+                } else {
+                    const lastNode = nth(this.foundation.foundationState.data.nodes, -1) as EditorNode;
+                    this.foundation.onUserInsertParagraph(lastNode.id);
+                }
+
+            } else {
+                const nodeId = ele.getAttribute("data-node-id");
+                this.foundation.onUserInsertParagraph(nodeId);
+            }
         }
+
         e.preventDefault();
         e.stopPropagation();
     }
@@ -135,7 +215,19 @@ class RichEditor extends BaseComponent<RichEditorProps, RichEditorState> {
         return (
             <div>
                 <Button onClick={this.foundation.onBold}>Bold</Button>
-                <div onClick={this.foundation.onContainerClick} suppressContentEditableWarning={true} className={"semi-rich-editor-container"} style={{ border: '1px solid red', width: 600, height: 400 }} ref={this.containerRef}>
+                <Button onClick={this.foundation.onItalic}>Italic</Button>
+                <Button onClick={this.foundation.onUnderline}>Underline</Button>
+                <input type={"color"} value={this.state.color} onChange={e => {
+                    this.setState({ color: e.target.value });
+                    this.foundation.onColor(e.target.value);
+                }}/>
+                <input type={"color"} value={this.state.bgColor} onChange={e => {
+                    this.setState({ bgColor: e.target.value });
+                    this.foundation.onBgColor(e.target.value);
+                }}/>
+                <div contentEditable={true}
+                    suppressContentEditableWarning={true} className={"semi-rich-editor-container"}
+                    style={{ border: '1px solid red', width: 600, height: 400 }} ref={this.containerRef}>
                     {this.state.elementList}
                 </div>
             </div>
