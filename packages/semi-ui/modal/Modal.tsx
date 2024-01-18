d2517caaebb742b7da9ce3dea6be21f590530b92
代强 <daiqianghaha@foxmail.com>
2024-01-18 11:42:01 +0800

    fix: fix enableBodyScroll when unmount

--- packages/semi-ui/modal/Modal.tsx
+++ packages/semi-ui/modal/Modal.tsx
@@ -243,6 +243,8 @@ class Modal extends BaseComponent<ModalReactProps, ModalState> {
     componentWillUnmount() {
         if (this.props.visible) {
             this.foundation.destroy();
+        } else {
+            this.foundation.enabledBodyScroll();
         }
     }
 
