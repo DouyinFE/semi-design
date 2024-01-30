c80cf9b9ad6c37e57d09d7a9c182c0dae579c93a
changlin0_0 <changlin2569@gmail.com>
2024-01-30 11:29:20 +0800

    feat: add renderCell for table rowSelection (#2045)
    * feat: add renderCell for table rowSelection
    
    * feat: added new exposure parameters and headers to support customization
    
    * fix: fix the header renderCell type problem
    
    * docs: update header renderCell document

--- packages/semi-ui/table/_story/table.stories.jsx
+++ packages/semi-ui/table/_story/table.stories.jsx
@@ -47,6 +47,7 @@ import ExpandAllGroupRows from './ExpandAllGroupRows';
 import ExpandRowByClick from './ExpandRowByClick';
 import FixAllColumnsWithoutWidth from './FixAllColumnsWithoutWidth';
 import HugeData from "./HugeData"
+import RowSelectionRenderCell from './RowSelectionRenderCell';
 
 export default {
   title: 'Table'
@@ -631,4 +632,10 @@ export const RenderPaginationDemo = () => <RenderPagination />;
 export const HugeDataDemo = ()=><HugeData/>
 HugeDataDemo.parameters = {
   chromatic: { disableSnapshot: true },
+};
+
+export const _RowSelectionRenderCell = () => <RowSelectionRenderCell />;
+
+_RowSelectionRenderCell.story = {
+  name: 'RowSelection RenderCell',
 };
