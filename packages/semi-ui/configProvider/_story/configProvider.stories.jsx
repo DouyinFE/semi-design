b3b022b1daf78d85c5bae926467df12738255815
pointhalo <point.halo@outlook.com>
2024-01-30 21:05:48 +0800

    test: add some test case

--- packages/semi-ui/configProvider/_story/configProvider.stories.jsx
+++ packages/semi-ui/configProvider/_story/configProvider.stories.jsx
@@ -4,43 +4,59 @@ import GetContainer from './GetPopupContainer';
 import RTLWrapper from './RTLDirection/RTLWrapper';
 import RTLTable from './RTLDirection/RTLTable';
 import RTLForm from './RTLDirection/RTLForm';
-import ConfigContext from "../context";
-import {Button, ConfigProvider} from "../../index";
+import ConfigContext from '../context';
+import { Button, ConfigProvider, Select, Tooltip, } from '../../index';
 
 export default {
-  title: 'ConfigProvider',
-  parameters: {
-    chromatic: { disableSnapshot: true },
-  },
-}
+    title: 'ConfigProvider',
+    parameters: {
+        chromatic: { disableSnapshot: true },
+    },
+};
 
-export {
-  ChangeTimeZone,
-  GetContainer,
-}
+export { ChangeTimeZone, GetContainer };
 
 export const RTLTableDemo = () => (
-  <RTLWrapper>
-    <RTLTable />
-  </RTLWrapper>
+    <RTLWrapper>
+        <RTLTable />
+    </RTLWrapper>
 );
 
 export const RTLFormDemo = () => (
-  <RTLWrapper>
-    <RTLForm />
-  </RTLWrapper>
+    <RTLWrapper>
+        <RTLForm />
+    </RTLWrapper>
 );
 
 ConfigProvider.overrideDefaultProps = {
     Button: {
-    type: 'warning',
-  },
-}
+        type: 'warning',
+    },
+    Select: {
+        zIndex: 2000,
+        getPopupContainer: () => document.querySelector('#popupContainer')
+    },
+    Tooltip: {
+        zIndex: 2001,
+        getPopupContainer: () => document.querySelector('#popupContainer')
+    },
+};
 
-export const DefaultPropsDemo = ()=>{
-    return <div>
-        <ConfigProvider >
-            <Button>test</Button>
-        </ConfigProvider>
-    </div>
-}
+export const DefaultPropsDemo = () => {
+    return (
+        <div>
+            <ConfigProvider>
+                <div style={{ position: 'relative'}} id='popupContainer'></div>
+                <Tooltip content="zindex 2001">
+                    <Button>test</Button>
+                </Tooltip>
+                <Select
+                    optionList={[
+                        { value: 1, label: 1 },
+                        { value: 2, label: 2 },
+                    ]}
+                ></Select>
+            </ConfigProvider>
+        </div>
+    );
+};
