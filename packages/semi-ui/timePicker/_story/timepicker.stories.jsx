5303da80238364ffc4e147b6b01ef40d7dc94abd
linyan <linyan.19960622@bytedance.com>
2024-02-21 15:51:30 +0800

    fix: when using timeZone and disabledHours at the same time in timepicker display value does not meet expectations

--- packages/semi-ui/timePicker/_story/timepicker.stories.jsx
+++ packages/semi-ui/timePicker/_story/timepicker.stories.jsx
@@ -1,6 +1,6 @@
 import React, { Component, useState } from 'react';
 import TimePickerPanel from '../index';
-import { TimePicker as BasicTimePicker, Button, Form, Popover } from '../../index';
+import { TimePicker as BasicTimePicker, Button, Form, Popover, ConfigProvider } from '../../index';
 import { strings } from '@douyinfe/semi-foundation/timePicker/constants';
 import { get } from 'lodash';
 
@@ -355,4 +355,23 @@ export const Fix1953 = () => {
   return (
     <TimePicker format={'HH'} defaultValue={'10'}/>
   );
+};
+
+export const Fix2082 = () => {
+  const [date, setDate] = useState(new Date());
+    return ( 
+        <ConfigProvider timeZone={10}>
+            <div style={{ width: 300 }}>
+                <h5 style={{ margin: 10 }}>TimePicker:</h5>
+                <TimePicker  
+                  disabledHours={v => [5]}   
+                  format="HH:mm" 
+                  value={date} 
+                  onChange={(date, dateString) => {
+                    console.log('日期', date);
+                    setDate(date)} }
+                />
+            </div>
+        </ConfigProvider>
+    );
 };
