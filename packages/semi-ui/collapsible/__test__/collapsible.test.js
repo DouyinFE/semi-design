de0b0bac474bcb71014f0c4e7f343e7df1e486b2
代强 <daiqianghaha@foxmail.com>
2024-03-25 19:55:24 +0800

    chore: change test

--- packages/semi-ui/collapsible/__test__/collapsible.test.js
+++ packages/semi-ui/collapsible/__test__/collapsible.test.js
@@ -16,29 +16,47 @@ describe('collapsible', () => {
         clear();
     });
 
-    it('keepDOM = true', async () => {
+    it('keepDOM = true',  () => {
         let component = getCollapsible({ keepDOM: true, motion: false, lazyRender:true });
         let collapsible = mount(component, { attachTo: document.getElementById('container') })
         expect(collapsible.exists(`.collapsible-test-content`)).toEqual(false);
-        // set true
-        collapsible.setProps({ isOpen: true });
-        collapsible.update(); // 必须调用一次update
+        collapsible.setProps({keepDOM: true, motion: false, lazyRender:false,isOpen:true})
+        collapsible.update();
         expect(collapsible.exists(`.collapsible-test-content`)).toEqual(true);
-        collapsible.setProps({ isOpen: false });
-        collapsible.update(); // 必须调用一次update
-        expect(collapsible.exists(`.collapsible-test-content`)).toEqual(false);
+        collapsible.unmount()
     });
 
-    it('keepDOM + lazyRender = false', () => {
-        let component = getCollapsible({ keepDOM: true, lazyRender: false });
+
+    it('keepDOM = true lazyRender=false', ()=>{
+        let component = getCollapsible({ keepDOM: true, motion: false, lazyRender:false });
+        let collapsible = mount(component, { attachTo: document.getElementById('container') })
+        expect(collapsible.exists(`.collapsible-test-content`)).toEqual(true);
+        collapsible.setProps({keepDOM: true, motion: false, lazyRender:false,isOpen:true})
+        collapsible.update();
+        expect(collapsible.exists(`.collapsible-test-content`)).toEqual(true);
+        collapsible.setProps({keepDOM: true, motion: false, lazyRender:false,isOpen:false})
+        collapsible.update();
+        expect(collapsible.exists(`.collapsible-test-content`)).toEqual(true);
+        collapsible.unmount()
+    })
+
+
+
+
+
+
+    it('keepDOM  lazyRender = false', () => {
+        let component = getCollapsible({ keepDOM: false,motion:false, lazyRender: false });
         let collapsible = mount(component);
-        expect(collapsible.exists(`.collapsible-test-content`)).toEqual(true);
+        expect(collapsible.exists(`.collapsible-test-content`)).toEqual(false);
         // set true
-        collapsible.setProps({ isOpen: true });
+        collapsible.setProps({ isOpen: true,motion:false,keepDOM: false, lazyRender: false });
         collapsible.update(); // 必须调用一次update
         expect(collapsible.exists(`.collapsible-test-content`)).toEqual(true);
-        collapsible.setProps({ isOpen: false });
+        collapsible.setProps({ isOpen: false,motion:false,keepDOM: false, lazyRender: false });
         collapsible.update(); // 必须调用一次update
-        expect(collapsible.exists(`.collapsible-test-content`)).toEqual(true);
+
+        expect(collapsible.exists(`.collapsible-test-content`)).toEqual(false);
+        collapsible.unmount()
     });
 });
