0142afeba80a637361369231c4020974d6fdd876
pointhalo <point.halo@outlook.com>
2024-05-07 11:37:07 +0800

    docs: add showcase: coze, update meego pic

--- src/sitePages/newHome/components/application/application.jsx
+++ src/sitePages/newHome/components/application/application.jsx
@@ -3,7 +3,7 @@ import { _t } from "src/utils/locale";
 import { Carousel } from '@douyinfe/semi-ui';
 import React, { useRef } from 'react';
 import styles from "./application.module.scss";
-import { AnyWebBigSvg, StarRiverBigSvg, CapCutBigSvg } from '../svg.jsx';
+import { AnyWebBigSvg, StarRiverBigSvg, CapCutBigSvg, CozeSvg } from '../svg.jsx';
 import { IconChevronLeft, IconChevronRight } from '@douyinfe/semi-icons';
 import classnames from 'classnames';
 
@@ -12,14 +12,15 @@ function Application(props) {
     const classForDarkImage = classnames(styles.showInDark, styles.hideInLight);
 
     const imgList = [
-        'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/feishuProject-page.png',
+        'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/meego.jpg',
         'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/douyinCreate-page.png',
+        'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/coze.jpg',
         'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/anyweb-page.png',
         'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/starRive-page.png',
         'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/capCut-page.png'
     ];
 
-    const imgInfo = [_t("feishu_program"), _t("douyin_creator"), _t("anyweb"), _t("star_river"), _t("cap_cut")];
+    const imgInfo = [_t("feishu_program"), _t("douyin_creator"), _t("coze"), _t("anyweb"), _t("star_river"), _t("cap_cut")];
 
     const logoNode = [
         <div key="lark" className={classnames(styles.group720)}>
@@ -33,13 +34,16 @@ function Application(props) {
             {/* eslint-disable-next-line */}       
             <img src="https://lf9-static.semi.design/obj/semi-tos/images/a50bd6f0-3474-11ec-b008-15e09471f238.png" className={classForDarkImage} height={64} width={275} />              
         </div>,
+        <div key='coze' className={classnames(styles.group720)}>
+            <CozeSvg />
+        </div>,
         <div key="anyWeb" className={classnames(styles.group720)}>
             <AnyWebBigSvg />
         </div>,
         <div key="starRiver" className={classnames(styles.group720)}>
             <StarRiverBigSvg />
         </div>,
-        <div key="clip" className={classnames(styles.group720)}>
+        <div key="capcut" className={classnames(styles.group720)}>
             <CapCutBigSvg />
         </div>  
     ];
