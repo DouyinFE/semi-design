f324b1a0773e98bc3babcb8bec2157e1b9848db7
DaiQiangReal <daiqianghaha@foxmail.com>
2024-06-18 18:34:12 +0800

    fix: use clone callbacks

--- packages/semi-foundation/utils/Event.ts
+++ packages/semi-foundation/utils/Event.ts
@@ -44,11 +44,9 @@ export default class Event {
         if (!this._eventMap.has(event)) {
             return false;
         }
-        const callbacks = this._eventMap.get(event);
-        // Reverse order for avoiding someone writing  the logic of deleting callback in callbacks into his or her callback code, for example the once func above
-        for (let i= callbacks.length - 1; i>=0; i--) {
-            callbacks[i](...args);
-        }
+        const callbacks = [...this._eventMap.get(event)];
+        // clone to avoid someone writing  the logic of deleting callback in callbacks into his or her callback code, for example the once func above
+        callbacks.forEach(callback => callback(...args));
         return true;
     }
 }
