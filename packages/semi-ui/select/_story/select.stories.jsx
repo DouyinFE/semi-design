f5be2f83257b6336ce226aa9dae8c218c010055d
pointhalo <88709023+pointhalo@users.noreply.github.com>
2024-06-27 13:45:40 +0800

    fix: select controled with same label text in reactNode, close #2284 (#2311)
    * fix: select controled with same label text in reactNode, close #2284
    * test: update e2e test

--- packages/semi-ui/select/_story/select.stories.jsx
+++ packages/semi-ui/select/_story/select.stories.jsx
@@ -3569,6 +3569,31 @@ export const UpdateOtherKeyNotInList = () => {
   );
 };
 
+
+export const ControledSameLabelInNode = () => {
+    const [value, setValue] = useState();
+    return <Select style={{ width: 180 }} 
+        value={value}
+        id='test'
+        // motion={false}
+        data-cy="singleControl"
+        onChange={(value) => {
+            console.log('change');
+            console.log(value)
+            setValue(value)
+        }}>
+        <Select.OptGroup label="Asia">
+            <Select.Option value="a-1" label={<div>China</div>} className='a-1' data-cy='a-1' key={'a-1'}></Select.Option>
+            <Select.Option value="a-2" label={<div>China</div>} className='a-2' data-cy='a-2' key={'a-2'}></Select.Option>
+            <Select.Option value="a-3" label={<div>Korea</div>} className='a-3'></Select.Option>
+        </Select.OptGroup>
+        <Select.OptGroup label="Europe">
+            <Select.Option value="b-1" label={<div>Germany</div>}></Select.Option>
+            <Select.Option value="b-2" label={<div>France</div>}></Select.Option>
+        </Select.OptGroup>
+    </Select>
+
+
 export const SearchPosition = () => {
   
   return (<>
