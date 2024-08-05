b96c22b7a68fb3db9b3196c99710c0c5fe11fe58
林艳 <linyan.19960622@bytedance.com>
2024-08-05 15:21:30 +0800

    feat: support side-by-side display of overlapping items in calendar components

--- packages/semi-foundation/slider/foundation.ts
+++ packages/semi-foundation/slider/foundation.ts
@@ -687,10 +687,6 @@ export default class SliderFoundation extends BaseFoundation<SliderAdapter> {
     }
 
     onFocus = (e: any, handler: 'min'| 'max') => {
-        const { tipFormatter } = this.getProps();
-        if (tipFormatter === null) {
-            return;
-        }
         handlePrevent(e);
         const { target } = e;
         try {
@@ -707,10 +703,6 @@ export default class SliderFoundation extends BaseFoundation<SliderAdapter> {
     }
 
     onBlur = (e: any, handler: 'min'| 'max') => {
-        const { tipFormatter } = this.getProps();
-        if (tipFormatter === null) {
-            return;
-        }
         const { firstDotFocusVisible, secondDotFocusVisible } = this.getStates();
         if (handler === 'min') {
             firstDotFocusVisible && this._adapter.setStateVal('firstDotFocusVisible', false);
