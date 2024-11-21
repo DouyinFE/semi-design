e101f3ce2394d3925ecff595e1fec932946cd1dd
shijia.me <shijia.me@bytedance.com>
2024-11-21 19:23:08 +0800

    feat: support shouldCellUpdate

--- packages/semi-ui/table/_story/Perf/index.jsx
+++ packages/semi-ui/table/_story/Perf/index.jsx
@@ -6,3 +6,4 @@ export { default as PerfContext } from './Render/context';
 export { default as PerfComplexRender } from './Render/complex';
 export { default as PerfResizableSelection } from './Render/resizableSelection';
 export { default as PerfOnRow } from './Render/onRow';
+export { default as DeepEqual } from './DeepEqual';
