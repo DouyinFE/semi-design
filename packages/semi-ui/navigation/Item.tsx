3bfe573a618ae8a99cbdfba7519992deb8e299c5
林艳 <linyan.19960622@bytedance.com>
2025-02-17 19:21:11 +0800

    fix: remove NavItem.children props

--- packages/semi-ui/navigation/Item.tsx
+++ packages/semi-ui/navigation/Item.tsx
@@ -19,7 +19,7 @@ import NavContext, { NavContextType } from './nav-context';
 import Dropdown from '../dropdown';
 
 const clsPrefix = `${cssClasses.PREFIX}-item`;
-interface NavItemProps extends ItemProps, BaseProps {
+interface NavItemProps extends ItemProps, Omit<BaseProps, 'children'> {
     disabled?: boolean;
     forwardRef?: (ele: HTMLLIElement) => void;
     icon?: React.ReactNode;
@@ -178,7 +178,6 @@ export default class NavItem extends BaseComponent<NavItemProps, NavItemState> {
     render() {
         const {
             text,
-            children,
             icon,
             toggleIcon,
             className,
@@ -202,8 +201,13 @@ export default class NavItem extends BaseComponent<NavItemProps, NavItemState> {
 
 
         let itemChildren = null;
-        if (!isNullOrUndefined(children)) {
-            itemChildren = children;
+        // Children is not a recommended usage and may cause some bug-like performance, but some users have already used it, so here we only delete the ts definition instead of deleting the actual code
+        // children 并不是我们推荐的用法，可能会导致一些像 bug的表现，但是有些用户已经用了，所以此处仅作删除 ts 定义而非删除实际代码的操作
+        // refer https://github.com/DouyinFE/semi-design/issues/2710
+        // @ts-ignore
+        if (!isNullOrUndefined(this.props?.children)) {
+            // @ts-ignore
+            itemChildren = this.props?.children;
         } else {
             let placeholderIcons = null;
             if (mode === strings.MODE_VERTICAL && !limitIndent && !isCollapsed) {
