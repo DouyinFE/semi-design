76b45df2f6278b03b48127f5cd1c0fc739ad4b8e
林艳 <linyan.19960622@bytedance.com>
2025-07-28 20:10:44 +0800

    fix: inputnumber plus calculate wrong cause by js precision

--- packages/semi-foundation/utils/number.ts
+++ packages/semi-foundation/utils/number.ts
@@ -10,7 +10,7 @@ export function plus(num1: number, num2: number) {
     const num1Digits = (num1.toString().split('.')[1] || '').length;
     const num2Digits = (num2.toString().split('.')[1] || '').length;
     const baseNum = Math.pow(10, Math.max(num1Digits, num2Digits));
-    return (num1 * baseNum + num2 * baseNum) / baseNum;
+    return (Math.round(num1 * baseNum + num2 * baseNum)) / baseNum;
 }
 
 export function minus(num1: number, num2: number) {
