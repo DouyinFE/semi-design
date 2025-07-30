fe920168d96ffb01e61ccaaf25c92fe9d17e179b
point.halo <point.halo@outlook.com>
2025-07-30 22:17:07 +0800

    fix: omitCss rule

--- packages/semi-rspack/src/rule.ts
+++ packages/semi-rspack/src/rule.ts
@@ -48,7 +48,7 @@ export function createThemeLoaderRule(opts?: SemiWebpackPluginOptions) {
 
 export function createOmitCssLoaderRule(_opts?: SemiWebpackPluginOptions) {
     return {
-        test: /@douyinfe(\/|\\)+semi-(ui|icons)(\/|\\)+.+\.js$/,
+        test: /@douyinfe\/semi-(ui|icons)\/lib\/.+\.js$/,
         use: [{ loader: OMIT_CSS_LOADER }],
     };
 }
