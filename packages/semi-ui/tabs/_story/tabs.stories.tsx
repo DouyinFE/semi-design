44840665c2c3af902b3be128c11017c4edceee72
DaiQiangReal <daiqianghaha@foxmail.com>
2026-04-30 11:25:24 +0800

    chore: remove Storybook demo for draggable tabs

--- packages/semi-ui/tabs/_story/tabs.stories.tsx
+++ packages/semi-ui/tabs/_story/tabs.stories.tsx
@@ -1,8 +1,6 @@
 import Demo from './Demo';
-import DraggableDemo from './DraggableDemo';
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 const stories = storiesOf('Tabs', module);
 stories.add('Tabs', () => <Demo />);
-stories.add('Draggable Tabs', () => <DraggableDemo />);
