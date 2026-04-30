80c7fe771ee69d4ae8cd97ffc9f78cebbac98c55
DaiQiangReal <daiqianghaha@foxmail.com>
2026-04-30 11:22:49 +0800

    fix(#2687): 【Auto】Feat: Add draggable tabs demo using renderTabBar with @dnd-kit
    This commit was automatically generated by Semi Issue Solver.
    Closes #2687

--- packages/semi-ui/tabs/_story/tabs.stories.tsx
+++ packages/semi-ui/tabs/_story/tabs.stories.tsx
@@ -1,6 +1,8 @@
 import Demo from './Demo';
+import DraggableDemo from './DraggableDemo';
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 const stories = storiesOf('Tabs', module);
 stories.add('Tabs', () => <Demo />);
+stories.add('Draggable Tabs', () => <DraggableDemo />);
